261cd9f11f2bef8a264d05415a7d81b7af1ac3dd
Artur Rodkin <das.modal@gmail.com>
2022-10-07 23:51:49 +0700

    add Message model, set relations, migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_163707) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_164745) do
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "token"
     t.datetime "created_at", null: false
@@ -23,4 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_07_163707) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
